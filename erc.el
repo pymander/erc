fc54000409df5f0e0a5ed179f2c495b70d052972
Jorgen Schaefer <forcer@users.sourceforge.net>
2005-02-02 01:52:22 +0000

    cscvs to tla changeset 1498
    Author: forcer
    Date: 2005/02/01 09:32:18
    * erc.el (erc-process-sentinel-1): Don't reconnect on connection
    refused. This error is reported differently when using
    open-network-stream-nowait.
    git-archimport-id: mwolson@gnu.org--2005/erc--cvs-head--0--patch-24

--- erc.el
+++ erc.el
@@ -79,7 +79,7 @@
 (require 'erc-backend)
 (require 'erc-menu)
 
-(defconst erc-version-string "Version 5.0 $Revision: 1.742 $"
+(defconst erc-version-string "Version 5.0 $Revision: 1.743 $"
   "ERC version.  This is used by function `erc-version'.")
 
 (defvar erc-official-location
@@ -2354,7 +2354,9 @@ action."
     (setq last-sent-time 0)
     (setq erc-lines-sent 0)
     (if (and erc-auto-reconnect
-	     (not (string= event "deleted\n")))
+	     (not (string= event "deleted\n"))
+	     ;; open-network-stream-nowait error for connection refused
+	     (not (string= event "failed with code 111\n")))
 	(erc erc-session-server erc-session-port current-nick
 	     erc-session-user-full-name t erc-session-password)
       ;; terminate, do not reconnect
