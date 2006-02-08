daf27478c818f6ae49860b640ab5428d7f974aef
Michael Olson <mwolson@gnu.org>
2006-02-08 01:53:49 +0000

    Update from CVS
    git-archimport-id: mwolson@gnu.org--2006/erc--cvs--0--patch-105

--- erc-stamp.el
+++ erc-stamp.el
@@ -38,7 +38,7 @@
 (require 'erc)
 (require 'erc-compat)
 
-(defconst erc-stamp-version "$Revision: 1.52 $"
+(defconst erc-stamp-version "$Revision: 1.53 $"
   "ERC stamp mode revision.")
 
 (defgroup erc-stamp nil
