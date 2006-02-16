f9841257fe7cd46f3a73b71606fabd690ba45d27
Michael Olson <mwolson@gnu.org>
2006-02-16 00:49:47 +0000

    Update from CVS
    git-archimport-id: mwolson@gnu.org--2006/erc--cvs--5.1--patch-24

--- erc.el
+++ erc.el
@@ -66,7 +66,7 @@
 
 ;;; Code:
 
-(defconst erc-version-string "Version 5.1.1 $Revision: 1.796.2.2 $"
+(defconst erc-version-string "Version 5.1.1 $Revision: 1.796.2.3 $"
   "ERC version.  This is used by function `erc-version'.")
 
 (eval-when-compile (require 'cl))
