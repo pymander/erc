d0434ba79332f7176fa7f6c6a4f9d9a80bb3fda9
Michael Olson <mwolson@gnu.org>
2006-08-03 03:25:06 +0000

    Release ERC 5.1.4.
    * erc.el (erc-version-string): Release ERC 5.1.4.
    
    * Makefile, NEWS, erc.texi: Update for the 5.1.4 release.
    git-archimport-id: erc@sv.gnu.org/erc--rel--5.1--patch-31

--- erc.el
+++ erc.el
@@ -67,7 +67,7 @@
 
 ;;; Code:
 
-(defconst erc-version-string "Version 5.1.3"
+(defconst erc-version-string "Version 5.1.4"
   "ERC version.  This is used by function `erc-version'.")
 
 (eval-when-compile (require 'cl))
