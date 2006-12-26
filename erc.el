bebbc11cd998de818300d9d126ad0d60648d5bc9
Michael Olson <mwolson@gnu.org>
2006-12-26 23:50:02 +0000

    Restore old point correctly when reconnecting
    2006-12-26  Michael Olson  <mwolson@gnu.org>
    
    	* erc.el (erc-open): Restore old point correctly, or at least get
    	closer to doing so than before.
    git-archimport-id: erc@sv.gnu.org/erc--main--0--patch-71

--- erc.el
+++ erc.el
@@ -1915,10 +1915,11 @@ Returns the buffer for the given server or channel."
 	(connected-p (unless connect erc-server-connected))
 	(buffer (erc-get-buffer-create server port channel))
 	(old-buffer (current-buffer))
-	(old-point (point))
+	old-point
 	continued-session)
     (erc-update-modules)
     (set-buffer buffer)
+    (setq old-point (point))
     (erc-mode)
     (setq erc-server-announced-name server-announced-name)
     (setq erc-server-connected connected-p)
