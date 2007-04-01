20700cd09e7c3520bfe3704aebe4051713b5a3e3
Michael Olson <mwolson@gnu.org>
2007-04-01 01:33:49 +0000

    Release ERC 5.2
    git-archimport-id: erc@sv.gnu.org/erc--main--0--patch-132

--- erc.el
+++ erc.el
@@ -66,7 +66,7 @@
 
 ;;; Code:
 
-(defconst erc-version-string "Version 5.2 (devel)"
+(defconst erc-version-string "Version 5.2"
   "ERC version.  This is used by function `erc-version'.")
 
 (eval-when-compile (require 'cl))
