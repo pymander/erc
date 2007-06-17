46c141aca684cdd8bbb8958048373c1365667232
Michael Olson <mwolson@gnu.org>
2007-06-17 15:43:48 +0000

    erc-pkg.el: Update description
    2007-06-17  Michael Olson  <mwolson@gnu.org>
    
    	* erc-pkg.el: Update description to match what is currently in
    	ELPA.
    git-archimport-id: erc@sv.gnu.org/erc--main--0--patch-150

--- erc-pkg.el
+++ erc-pkg.el
@@ -1,4 +1,3 @@
 ;; ELPA package definition for ERC
 
-(define-package "erc" "5.2"
-  "IRC client that is powerful, modular, and extensible")
+(define-package "erc" "5.2" "An Emacs Internet Relay Chat client")
